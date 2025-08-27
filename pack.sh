f0e44a04ff5381c2cc9042450e424652aea02c63
4cf93337247e3acd <4f6fd3ffb7474b04>
2025-08-27 03:47:50 +0000

    rename output file

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
-rm -rf prototype.zip
-zip -r prototype.zip agent Dockerfile requirements.txt main.py mcps greeting.txt public
+rm -rf market-research.zip
+zip -r market-research.zip agent Dockerfile requirements.txt main.py mcps greeting.txt public
