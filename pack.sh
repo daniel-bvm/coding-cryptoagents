ec437cbe4bf9991002607843e69236ffab419060
ac6fc41494819ae5 <a51048a756a4294b>
2025-08-29 13:16:41 +0000

    update

--- pack.sh
+++ pack.sh
@@ -1,4 +1,6 @@
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
-rm -rf prototype.zip
-zip -r prototype.zip agent Dockerfile requirements.txt main.py mcps greeting.txt public
+output_file_name="prototype.zip"
+rm -rf $output_file_name
+zip -r $output_file_name agent Dockerfile requirements.txt main.py mcps greeting.txt public
+echo "Packed $output_file_name"
